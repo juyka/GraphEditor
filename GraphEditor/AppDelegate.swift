ed7b7641a06e9b15024d982f4c54e87851e08c82
Анастасия Васюра <a.vasyura@rambler-co.ru>
2016-08-31 09:39:07 +0300

    Initial Commit

---
+++ GraphEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GraphEditor
+//
+//  Created by Анастасия Васюра on 31/08/16.
+//  Copyright © 2016 Анастасия Васюра. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GraphEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GraphEditor
+//
+//  Created by Анастасия Васюра on 31/08/16.
+//  Copyright © 2016 Анастасия Васюра. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
